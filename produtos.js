96a242be99a9b2f0b5cb27b3adb44094c4c7568d
Nay semi jóias CEO <semijoiasnay451@gmail.com>
2026-09-13 16:49:49 -0300

    Update WhatsApp link in produtos.js

--- produtos.js
+++ produtos.js
@@ -106,7 +106,7 @@ botaoWhatsApp.addEventListener("click", function() {
         " - Banho: " + produto.cores.join(" • ");
 
     window.open(
-        "https://wa.me/5511999999999?text=" + encodeURIComponent(mensagem),
+        "https://wa.me/5519992763019?text=" + encodeURIComponent(mensagem),
         "_blank"
     );
 });
